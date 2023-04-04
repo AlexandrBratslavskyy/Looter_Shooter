22506a9ec246eed1201f87643202863c1bf8c9b7
Alex <35470085+AlexandrBratslavskyy@users.noreply.github.com>
2023-04-03 22:53:32 -0700

    Basic Move

---
+++ PlayerMovement.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+var mouseSensibility = 1200
+var mouse_relative_x = 0
+var mouse_relative_y = 0
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	# Handle Shooting
+	if Input.is_action_just_pressed("Shoot"):
+		print("bang")
+		# shoot()
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
