7b94acd2fbad843af746efe789aadd5c847c090a
Alex <35470085+AlexandrBratslavskyy@users.noreply.github.com>
2023-04-08 17:04:34 -0700

    take damage bare bones

---
+++ Enemy/Enemy.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+@export var health: Health
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready() -> void:
+	health.current_health.connect(change_health)
+	health.no_cur_health.connect(no_health)
+
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y -= gravity * delta
+#
+#	# Handle Jump.
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+#	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+#	if direction:
+#		velocity.x = direction.x * SPEED
+#		velocity.z = direction.z * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+#	move_and_slide()
+
+func take_damage() -> void:
+	print("enemy")
+	health.decrease_health()
+	
+func change_health(value: int) -> void:
+	print(value)
+	
+func no_health() -> void:
+	print("dead")
---
+++ Player/PlayerMovement.gd
@@ -0,0 +1,70 @@
+extends CharacterBody3D
+
+@onready var cam: Camera3D = $Head/Camera
+@onready var gunRay: RayCast3D = $Head/Camera/RayCast3D
+
+@export var health: Health
+
+var mouseSensibility = 120
+var mouse_relative_x = 0
+var mouse_relative_y = 0
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Captures mouse
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	gunRay.add_exception(self)
+	print(health.cur_health)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	# Release mouse to escape
+	if Input.is_action_just_pressed("escape"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func _physics_process(delta) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Handle Shooting
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func _input(event) -> void:
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x / mouseSensibility
+		cam.rotation.x -= event.relative.y / mouseSensibility
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(90) )
+		mouse_relative_x = clamp(event.relative.x, -50, 50)
+		mouse_relative_y = clamp(event.relative.y, -50, 10)
+		
+func shoot() -> void:
+	if not gunRay.is_colliding():
+		return
+	var cl := gunRay.get_collider()
+	
+	if cl.has_method("take_damage"):
+		cl.take_damage()
